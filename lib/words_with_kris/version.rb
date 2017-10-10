70920af4ece5b743f889c57ad10f8be3ed6a283f
Kris Gray <kris.m.gray@gmail.com>
2017-10-10 10:10:58 -0600

    began practice open source

---
+++ lib/words_with_kris/version.rb
@@ -0,0 +1,3 @@
+module WordsWithKris
+  VERSION = "0.1.0"
+end
---
+++ spec/words_with_kris_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe WordsWithKris do
+  subject { WordsWithKris }
+
+  it "has a version number" do
+    expect(WordsWithKris::VERSION).not_to be nil
+  end
+
+  describe 'manipulate strings' do
+    let(:str) { 'My String' }
+
+    it 'reverses a string' do
+      expect(subject.reversify(str)).to eq('gnirtS yM')
+    end
+
+    it 'adds spaces' do
+      expect(subject.spacify(str, 1)).to eq('M y  S t r i n g')
+    end
+  end
+
+end
